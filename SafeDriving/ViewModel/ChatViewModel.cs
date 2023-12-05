4cdb988873176778c482dd6a52f669153efbdf73
Orex-x <orexkashtan@gmail.com>
2023-12-06 00:25:27 +0300

    не доделал чаты

--- SafeDriving/ViewModel/ChatViewModel.cs
+++ SafeDriving/ViewModel/ChatViewModel.cs
@@ -1,14 +1,37 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Collections.ObjectModel;
 namespace SafeDriving.ViewModel
 {
     public partial class ChatViewModel : ObservableObject
     {
+        [ObservableProperty]
+        ObservableCollection<MessageViewModel> messages;
 
+        public ChatViewModel() 
+        {
+            var list = new List<MessageViewModel>
+            {
+                new MessageViewModel
+                {
+                    Body = "Привет",
+                    DateTime = DateTime.Now,
+                    Horizontal = "Start",
+                },
+                new MessageViewModel
+                {
+                    Body = "Здарова",
+                    DateTime = DateTime.Now.AddMinutes(1),
+                    Horizontal = "End",
+                },
+                 new MessageViewModel
+                {
+                    Body = "AKLSDJFKLASDJFLKJASDLKFJLASDKJFLKASDJFLKAJSDDLFKJASDLKFJSLADKJFLAKSDJFLKASDJFLKJASDLKFJASDLKJFKL",
+                    DateTime = DateTime.Now.AddMinutes(1),
+                    Horizontal = "Start",
+                },
+            };
+
+            Messages = new(list);
+        }
     }
 }
---
+++ SafeDriving/ViewModel/MessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SafeDriving.ViewModel
+{
+    public partial class MessageViewModel : ObservableObject
+    {
+        public string Body { get; set; }
+        public DateTime DateTime { get; set; }
+
+        [ObservableProperty]
+        public string horizontal;
+    }
+}
