eb0551e54192eb8fb2691b0f4d4f9a6acf02ac60
Orex-x <vipboy78038@gmail.com>
2023-12-10 21:52:57 +0300

    сделал лист с чатами

---
+++ SafeDriving/ViewModel/ChatItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SafeDriving.ViewModel
+{
+    public partial class ChatItemViewModel : ObservableObject
+    {
+        public int Id { get; set; }
+
+        [ObservableProperty]
+        string chatName;
+
+        [ObservableProperty]
+        string lastMessage;
+    }
+}
--- SafeDriving/ViewModel/ListChatsViewModel.cs
+++ SafeDriving/ViewModel/ListChatsViewModel.cs
@@ -1,13 +1,46 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SafeDriving.Pages;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SafeDriving.ViewModel
 {
     public partial class ListChatsViewModel : ObservableObject
     {
+        [ObservableProperty]
+        ObservableCollection<ChatItemViewModel> chats;
+
+
+        [RelayCommand]
+        async Task TapItem(ChatItemViewModel vm)
+        {
+            await Shell.Current.GoToAsync($"{nameof(ChatPage)}?id={vm.Id}");
+        }
+        
+        
+        public ListChatsViewModel()
+        {
+            chats = new ObservableCollection<ChatItemViewModel>
+            {
+                new ChatItemViewModel()
+                {
+                    Id = 1,
+                    ChatName = "Ира Пронина",
+                    LastMessage = "оаыадлиовалям мваораичжы "
+                },
+                new ChatItemViewModel()
+                {
+                    Id = 2,
+                    ChatName = "Главный чел",
+                    LastMessage = "оаыадлиовалям мваораичжы sdjfhjsd sjdfhjsdhfj sjdfhjshdfkj"
+                }
+            };
+        }
     }
 }
